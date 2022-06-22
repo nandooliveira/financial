e2d74f61978e2b94724125d9e57f5b7322953d4b
Fernando Oliveira <nandooliveira.al@gmail.com>
2022-06-21 23:04:22 -0300

    Improve configuration

--- lib/hello_world.rb
+++ lib/hello_world.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class HelloWorld
- attr_reader :coolness
+  attr_reader :coolness
 
- def initialize
-   @coolness = 11
- end
+  def initialize
+    @coolness = 11
+  end
 end
